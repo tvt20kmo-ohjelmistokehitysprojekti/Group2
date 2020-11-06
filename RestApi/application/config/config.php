6a3016883aca4bc07abbc24667a0d75037637acb
Visa Simula <visa.simula@gmail.com>
2020-11-06 21:41:35 +0200

    user, user_model & login

--- RestApi/application/config/config.php
+++ RestApi/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/RestApi';               //Change when switching to mysli.oamk.fi db
+$config['base_url'] = 'http://localhost/RestApi';               //Change when switching to mysli.oamk.fi db -VS
 //$config['base_url'] = 'https://www.students.oamk.fi/RestApi';
 
 /*
