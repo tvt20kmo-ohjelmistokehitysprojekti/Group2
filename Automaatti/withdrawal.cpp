ee8c2dc525ca76385583d618cabcd76c793fd623
Jasmiina Mikkola <mikkola.jasmiina@gmail.com>
2020-11-07 16:16:54 +0200

    edit2

--- Automaatti/withdrawal.cpp
+++ Automaatti/withdrawal.cpp
@@ -12,3 +12,34 @@ withdrawal::~withdrawal()
 {
     delete ui;
 }
+
+void withdrawal::on_pushButton_20e_clicked()
+{
+
+   //Lisää tähän 20e-napin toiminnot (nostetaan 20e)
+}
+
+void withdrawal::on_pushButton_40e_clicked()
+{
+    //Lisää tähän 40e-napin toiminnot (nostetaan x euroa)
+}
+
+void withdrawal::on_pushButton_50e_clicked()
+{
+    //Lisää tähän 50e-napin toiminnot (nostetaan x euroa)
+}
+
+void withdrawal::on_pushButton_100e_clicked()
+{
+    //Lisää tähän 100e-napin toiminnot (nostetaan x euroa)
+}
+
+void withdrawal::on_pushButton_enterAmount_clicked()
+{
+    //Lisää tähän "Muu summa" -napin toiminnot (nostetaan x euroa)
+}
+
+void withdrawal::on_pushButton_menu_clicked()
+{
+    //Lisää tähän toiminnot, jolla päästään withdrawal-valikosta menu-valikkoon
+}
