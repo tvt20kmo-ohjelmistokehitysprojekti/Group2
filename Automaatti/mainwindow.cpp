8a79450a8ef4c29e58a4d90e3426b230df83800a
Jasmiina Mikkola <mikkola.jasmiina@gmail.com>
2020-11-13 18:22:22 +0200

    login testing

--- Automaatti/mainwindow.cpp
+++ Automaatti/mainwindow.cpp
@@ -1,5 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QtNetwork/QNetworkRequest>
+#include <QtNetwork/qnetworkrequest.h>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/qnetworkaccessmanager.h>
+#include <QtNetwork/qnetworkreply.h>
+#include <QJsonDocument>
+#include <qjsondocument.h>
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -15,5 +23,51 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_login_clicked()
 {
-    //Lisää tähän kirjaudu-napin toiminnot
+    QString card_id, card_pin;
+    card_id=ui->lineEdit_cardID->text();
+    card_pin=ui->lineEdit_PIN->text();
+
+    QNetworkRequest request(QUrl("http://localhost/RestApi/index.php/api/login/check_login/?card_id"+card_id+"&card_pin"+card_pin));
+        request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+        //Authenticate
+        QString username="admin";
+        QString password="1234";
+        QString concatenatedCredentials = username + ":" + password;
+            QByteArray data = concatenatedCredentials.toLocal8Bit().toBase64();
+            QString headerData = "Basic " + data;
+            request.setRawHeader("Authorization", headerData.toLocal8Bit() );
+
+        QNetworkAccessManager nam;
+        QNetworkReply *reply = nam.get(request);
+        while (!reply->isFinished())
+        {
+            qApp->processEvents();
+        }
+        QByteArray response_data = reply->readAll();
+
+        qDebug()<<"DATA:"+response_data;
+
+        reply->deleteLater();
+
+        /*if(response_data.compare("true")==0)
+        {
+            ui->label_test->setText("Right");
+        }
+        else
+        {
+            ui->label_test->setText("Wrong");
+        }*/
+
+        if(response_data=="true")
+        {
+            ui->label_test->setText("Right");
+        }
+        else
+        {
+            ui->label_test->setText("Wrong");
+        }
+
+
+
+
 }
